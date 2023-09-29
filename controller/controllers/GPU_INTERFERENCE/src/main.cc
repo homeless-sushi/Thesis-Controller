d6fae659c2e17a68a092e1dcee5d0ae90ac09295
Guido Rolle <rolleguido@gmail.com>
2023-09-29 17:54:25 +0200

    Add GPU_INTERFERENCE controller

---
+++ controller/controllers/GPU_INTERFERENCE/src/main.cc
@@ -0,0 +1,79 @@
+#include "Policies/GpuInterferencePolicy.h"
+
+#include <chrono>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <thread>
+
+#include <csignal>
+
+#include "AppRegisterServer/Policy.h"
+
+#include <boost/program_options.hpp>
+
+#define N_CORES 4
+#define CONTROL_PERIOD 1000
+
+namespace po = boost::program_options;
+po::options_description SetupOptions();
+
+void SetupSignals();
+
+bool stop = false;
+
+int main(int argc, char *argv[])
+{
+    SetupSignals();
+
+    po::options_description desc(SetupOptions());
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    if (vm.count("help"))
+    {
+        std::cout << desc << "\n";
+        return 0;
+    }
+
+    std::string controllerLogUrl(vm["controller-log"].as<std::string>());
+    std::unique_ptr<Policy::Policy> policy(
+        new Policy::GpuInterferencePolicy(
+            N_CORES,
+            controllerLogUrl
+    ));
+
+    unsigned int i = 0;
+    while(!stop){
+        policy->run(i);
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(CONTROL_PERIOD));
+
+        i++;
+    }
+}
+
+po::options_description SetupOptions()
+{
+    po::options_description desc("Allowed options");
+    desc.add_options()
+    ("help", "Display help message")
+    ("controller-log,L", po::value<std::string>(), "the URL to output the controller log to")
+    ;
+
+    return desc;
+}
+
+void SetupSignals()
+{
+    auto stopController = [](int signal){
+        std::cerr << std::endl;
+        std::cerr << "Received signal: " << signal << std::endl;
+        std::cerr << "Stopping controller" << std::endl;
+
+        stop = true;
+    };
+
+    std::signal(SIGINT, stopController);
+    std::signal(SIGTERM, stopController);
+
+}
