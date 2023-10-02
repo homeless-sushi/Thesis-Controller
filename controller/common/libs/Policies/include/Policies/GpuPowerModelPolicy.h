ff6f39b5713b48f2fd4e0ee30c8054efe17bbad1
Guido Rolle <rolleguido@gmail.com>
2023-10-02 11:49:43 +0200

    Add GpuPowerModelPolicy

---
+++ controller/common/libs/Policies/include/Policies/GpuPowerModelPolicy.h
@@ -0,0 +1,37 @@
+#include <fstream>
+#include <set>
+#include <string>
+
+#include <sys/types.h>
+
+#include "AppRegisterServer/Policy.h"
+#include "AppRegisterServer/Sensors.h"
+#include "AppRegisterServer/Utilization.h"
+
+namespace Policy
+{
+    class GpuPowerModelPolicy : public Policy
+    {
+        private:
+            std::set<pid_t> newRegisteredApps; /**< Apps that have been added this cycle */
+            std::set<pid_t> runningApps;       /**< Apps that have been running */
+            
+            std::fstream controllerLogFile;
+
+            Utilization::Utilization utilization;
+
+            Sensors::Sensors sensors;
+            std::fstream sensorLogFile;
+            unsigned nFreqSamples;
+            unsigned currFreqSamples = 0;
+
+        public:
+            GpuPowerModelPolicy(
+                unsigned nCores,
+                std::string controllerLogUrl,
+                unsigned nFreqSamples,
+                std::string sensorsLogUrl);
+
+            void run(int cycle) override;
+    };
+}
